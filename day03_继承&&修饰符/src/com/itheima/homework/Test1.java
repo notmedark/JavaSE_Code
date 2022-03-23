e3ba445980451c6dfefdbe801fe7b71dd4a795bb
kcll <1150886240@qq.com>
2022-03-24 00:00:21 +0800

    hot-fix commit1

--- day03_继承&&修饰符/src/com/itheima/homework/Test1.java
+++ day03_继承&&修饰符/src/com/itheima/homework/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
         System.out.println(stu.getInfo());
         System.out.println("2222222222");
         System.out.println("33333333333333");
+        System.out.println("33333333333333444");
     }
 }
 
